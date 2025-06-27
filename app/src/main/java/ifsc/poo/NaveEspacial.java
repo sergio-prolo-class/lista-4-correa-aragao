5146ba5aaa11ef5560566b5c5935129090ba5c47
b8llacorrea <artemisfall8@gmail.com>
2025-06-26 21:55:41 -0300

    começar lista 4 - interfaces

---
+++ app/src/main/java/ifsc/poo/NaveEspacial.java
@@ -0,0 +1,12 @@
+package ifsc.poo;
+
+// Classe base abstrata
+public abstract class NaveEspacial {
+    protected int velocidadeAtual;
+
+    public NaveEspacial(int velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual;
+    }
+
+    //public abstract int
+}
